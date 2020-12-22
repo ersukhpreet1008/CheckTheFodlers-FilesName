606743e4e53f0de5a51f7cff5712064c3b8a31e4
ersukhpreet1008 <76466771+ersukhpreet1008@users.noreply.github.com>
2020-12-22 13:12:06 +0530

    Create Code to check folders & files details

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FolderFileListCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FoldersInDirectory(@"D:\");
+            Console.WriteLine("Press Enter to Continue");
+            Console.Read();
+
+            FilesInDirectory(@"D:\");
+            Console.WriteLine("Press Enter to Continue");
+            Console.Read();
+
+        }
+
+        static void FoldersInDirectory(string workingDirectory)
+        {
+            string[] folderPaths = Directory.GetDirectories(workingDirectory);
+         
+
+            foreach (string folderPath in folderPaths)
+            {
+                Console.WriteLine(folderPath);
+            }
+        }
+
+        static void FilesInDirectory(string workingFiles)
+        {
+            string[] filePaths = Directory.GetFiles(workingFiles);
+
+
+            foreach (string filePath in filePaths)
+            {
+                Console.WriteLine(filePath);
+            }
+        }
+
+    }
+}
